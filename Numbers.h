25416a99e7b256ac6cb24532dba47f53498dfa3e
alexeyk1241 <52071640+alexeyk1241@users.noreply.github.com>
2019-06-21 20:41:12 +0700

    2.2

---
+++ Numbers.h
@@ -0,0 +1,30 @@
+enum type { COMPLEX, SIMPLE };
+struct complex {
+	type key;
+	float real;
+	float imaginary;
+};
+struct simple {
+
+	type key;
+	float numerator;
+	float denominator;
+};
+struct numbers {
+	type key;
+};
+
+struct Node {
+	Node() {
+		this->Next = nullptr;
+		this->Prev = nullptr;
+		this->data = nullptr;
+	};
+	Node* Next;
+	Node* Prev;
+	numbers* data;
+};
+struct container {
+	Node* Top;
+	int count;
+};
